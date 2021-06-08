5e8ab91b392ab8b335af37ee1c3e5e196d1d184a
Alexander Wu <alexanderwu2000@gmail.com>
2021-06-08 12:17:36 -0500

    Init Commit

---
+++ src/data.js
@@ -0,0 +1,85 @@
+export const data1 = [
+  {
+    id: 1,
+    title: "Card One",
+    body: "question 1",
+  },
+  {
+    id: 2,
+    title: "Card Two",
+    body: "question 2",
+  },
+  {
+    id: 3,
+    title: "Card Three",
+    body: "question 3",
+  },
+  {
+    id: 4,
+    title: "Card Four",
+    body: "question 4",
+  },
+  {
+    id: 5,
+    title: "Card Five",
+    body: "question 5",
+  },
+  {
+    id: 6,
+    title: "Card Six",
+    body: "question 6",
+  },
+  {
+    id: 7,
+    title: "Card Seven",
+    body: "question 7",
+  },
+  {
+    id: 8,
+    title: "Card Eight",
+    body: "question 8",
+  },
+  {
+    id: 9,
+    title: "Card Nine",
+    body: "question 9",
+  },
+  {
+    id: 10,
+    title: "Card Ten",
+    body: "question 10",
+  },
+];
+
+export const data2 = [
+  {
+    id: 1,
+    label: "Day One",
+    times: [23],
+  },
+  {
+    id: 2,
+    label: "Day Two",
+    times: [2, 4, 18, 20, 23],
+  },
+  {
+    id: 3,
+    label: "Day Three",
+    times: [2, 4, 18, 20, 23],
+  },
+  {
+    id: 4,
+    label: "Day Four",
+    times: [2, 4, 18, 20],
+  },
+  {
+    id: 5,
+    label: "Day Five",
+    times: [17, 19, 22],
+  },
+  {
+    id: 6,
+    label: "Day Six",
+    times: [1, 17, 19, 22],
+  },
+];
